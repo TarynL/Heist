e2aae4e951e5039e499cfc1a4acfe66df559d388
ColtMay <colten_mayberry@pm.me>
2021-06-01 11:06:55 -0500

    allows adding multiple members until blank

--- Program.cs
+++ Program.cs
@@ -8,24 +8,42 @@ namespace Heist
         static void Main(string[] args)
         {
             Console.WriteLine("Plan Your Heist!");
+            bool NameCheck = true;
 
+            List<TeamMember> TeamMemberList = new List<TeamMember>();
 
-            Console.Write("Please enter team members first name: ");
-            string MemberFirstName = Console.ReadLine();
-            Console.Write("Please enter team members last name: ");
-            string MemberLastName = Console.ReadLine();
-            Console.Write("Please enter skill level: ");
-            int MemberSkillLevel = int.Parse(Console.ReadLine());
-            Console.Write("Please enter a courage factor between 0.0-2.0: ");
-            double MemberCourageFactor = double.Parse(Console.ReadLine());
-
-            TeamMember Colten = new TeamMember()
+            while (NameCheck)
             {
-                FirstName = MemberFirstName,
-                LastName = MemberLastName,
-                SkillLevel = MemberSkillLevel,
-                CourageFactor = MemberCourageFactor
-            };
+                Console.Write("Please enter team members first name: ");
+                string MemberFirstName = Console.ReadLine();
+                if (MemberFirstName == "")
+                {
+                    break;
+                }
+                else
+                {
+                    NameCheck = true;
+                }
+                Console.Write("Please enter team members last name: ");
+                string MemberLastName = Console.ReadLine();
+                Console.Write("Please enter skill level: ");
+                int MemberSkillLevel = int.Parse(Console.ReadLine());
+                Console.Write("Please enter a courage factor between 0.0-2.0: ");
+                double MemberCourageFactor = double.Parse(Console.ReadLine());
+
+                TeamMember newMember = new TeamMember()
+                {
+                    FirstName = MemberFirstName,
+                    LastName = MemberLastName,
+                    SkillLevel = MemberSkillLevel,
+                    CourageFactor = MemberCourageFactor
+                };
+
+                TeamMemberList.Add(newMember);
+                Console.WriteLine($"Name: {newMember.FullName()} \nSkill Level: {newMember.SkillLevel} \nCourage Factor: {newMember.CourageFactor}");
+            }
+
+
             TeamMember Laurel = new TeamMember()
             {
                 FirstName = "Laurel",
@@ -41,14 +59,13 @@ namespace Heist
                 CourageFactor = 1.9
             };
 
-
-            Console.WriteLine($"Name: {Colten.FullName()} \nSkill Level: {Colten.SkillLevel} \nCourage Factor: {Colten.CourageFactor}");
-
-            List<TeamMember> TeamMemberList = new List<TeamMember>();
-
-            TeamMemberList.Add(Colten);
             TeamMemberList.Add(Laurel);
             TeamMemberList.Add(Taryn);
+
+            foreach (TeamMember member in TeamMemberList)
+            {
+                Console.WriteLine(member.FullName());
+            }
 
         }
     }
