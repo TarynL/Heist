2f5c64684291753e27d6ee3a0eb591971429adb1
TarynL <tarlytle@gmail.com>
2021-06-01 15:22:49 -0500

    mario tunes

---
+++ Music.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Threading;
+
+namespace Heist
+{
+    public class Music
+    {
+        public void Mario()
+        {
+            Console.Beep(659, 150); Console.Beep(659, 150); Thread.Sleep(150);
+            Console.Beep(659, 150); Thread.Sleep(167); Console.Beep(523, 150);
+            Console.Beep(659, 150); Thread.Sleep(150); Console.Beep(784, 150);
+            Thread.Sleep(375); Console.Beep(392, 150);
+        }
+    }
+}
